720b7e9ca7a1cd6f062ff2995ade8e0940a66edf
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-07-08 17:11:47 +0200

    update swagger ui

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,16 +3,17 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: FUSIO_URL,
+    url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
-      SwaggerUIBundle.presets.apis
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
     ],
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "BaseLayout"
+    layout: "StandaloneLayout"
   });
 
   //</editor-fold>
